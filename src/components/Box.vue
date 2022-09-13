be1f1751d6499dfaf15ea4c459c760ff06d82563
lventura <lventura@uolinc.com>
2022-09-13 17:28:57 -0300

    All project

---
+++ src/components/Box.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box has-text-weight-bold" :style="estilos">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: 'Box',
+  data() {
+    return {
+      estilos: {
+        backgroundColor: '#FAF0CA'
+      }
+    }
+  }
+})
+</script>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,11 +1,53 @@
 <template>
-$END$
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+    <button class="button" @click="iniciar" :disabled="cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-play" />
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
+      <span class="icon">
+        <i class="fas fa-stop" />
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
 </template>
 
-<script>
-export default {
-name: "Temporizador"
-}
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "@/components/Cronometro.vue";
+
+export default defineComponent({
+  name: "Temporizador",
+  emits: ['aoTemporizadorFinalizado'],
+  components: {
+    Cronometro
+  },
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+      cronometroRodando: false
+    }
+  },
+  methods: {
+    iniciar() {
+      this.cronometroRodando = true;
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+    finalizar() {
+      this.cronometroRodando = false;
+      clearInterval(this.cronometro);
+      this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos);
+      this.tempoEmSegundos = 0;
+    }
+  }
+})
 </script>
 
 <style scoped>
